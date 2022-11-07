e9c94585be354ec4cdd8dd76b86fd376d3b9946e
tincho <tincho_simon@hotmail.com>
2022-11-07 14:03:40 +0100

    hover button details

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ function submit() {
 
 function rating(n) {
     if (document.getElementById('rating-selected').innerHTML != '') {
-        document.getElementById(document.getElementById('rating-selected').innerHTML).classList.remove('selected-button');
-        document.getElementById(document.getElementById('rating-selected').innerHTML).style.color = 'hsl(217, 12%, 63%)';  
+        document.getElementById(document.getElementById('rating-selected').innerHTML).classList.remove('selected-button'); 
     }
     document.getElementById(n).classList.add('selected-button');
-    document.getElementById(n).style.color = '#ffffff';
     document.getElementById('rating-selected').innerHTML = n
 }
