5329ba27f6810c32c88098d770023de9f396df76
tincho <tincho_simon@hotmail.com>
2022-11-07 12:51:40 +0100

    selected & unselected buttons

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ function submit() {
     if (document.getElementById('rating-selected').innerHTML != ''){
         document.getElementById('rating-container').style.display = 'none';
         document.getElementById('thanks-container').style.display = 'flex';
-        console.log(document.activeElement)
     }
 }
 
 function rating(n) {
     if (document.getElementById('rating-selected').innerHTML != '') {
-        document.getElementById(document.getElementById('rating-selected').innerHTML).style.backgroundColor = '#2E3845';
+        document.getElementById(document.getElementById('rating-selected').innerHTML).classList.remove('selected-button');
         document.getElementById(document.getElementById('rating-selected').innerHTML).style.color = 'hsl(217, 12%, 63%)';  
     }
-    document.getElementById(n).style.backgroundColor = 'hsl(25, 97%, 53%)';
+    document.getElementById(n).classList.add('selected-button');
     document.getElementById(n).style.color = '#ffffff';
     document.getElementById('rating-selected').innerHTML = n
 }
