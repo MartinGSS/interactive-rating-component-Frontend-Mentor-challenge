417e26b2ca2c53a4dd2b60da2b65d8c4a8f0fe85
tincho <tincho_simon@hotmail.com>
2022-11-07 01:43:07 +0100

    second review

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
- function submit() {
-    document.getElementById('rating-container').style.display = 'none';
-    document.getElementById('thanks-container').style.display = 'flex';
+function submit() {
+    if (document.getElementById('rating-selected').innerHTML != ''){
+        document.getElementById('rating-container').style.display = 'none';
+        document.getElementById('thanks-container').style.display = 'flex';
+        console.log(document.activeElement)
+    }
 }
 
 function rating(n) {
-    let rating = 0;
+    if (document.getElementById('rating-selected').innerHTML != '') {
+        document.getElementById(document.getElementById('rating-selected').innerHTML).style.backgroundColor = '#2E3845';
+        document.getElementById(document.getElementById('rating-selected').innerHTML).style.color = 'hsl(217, 12%, 63%)';  
+    }
+    document.getElementById(n).style.backgroundColor = 'hsl(25, 97%, 53%)';
+    document.getElementById(n).style.color = '#ffffff';
     document.getElementById('rating-selected').innerHTML = n
 }
